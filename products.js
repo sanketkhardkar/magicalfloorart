e09269be37eb7eb93025948c45871bd55e456ee3
sanketkhardkar <86860065+sanketkhardkar@users.noreply.github.com>
2026-08-21 19:22:54 +0530

    Add files via upload

--- products.js
+++ products.js
@@ -1,22 +1,41 @@
 // products.js
 const products = [
-	{id: 1, 
-		name: "Gauri Pujan & Mahalakshmi Acrylic Rangoli Set", 
-		description: "Complete Gauri Pujan acrylic set featuring twin divine faces, pink lotuses, samai lamps, and auspicious footprints. Reusable, washable, and mess-free for effortless Mahalakshmi and Gauri Ganpati celebrations.", 
-		price: 1000, 
-		mrp: 1500, 
-		badge: "Special Deal", 
-		size: '12" x 12"', 
-		height: "2 mm", 
-		weight: "100g", 
-		images: [
-			"images/product1-1.jpg", 
-			"images/product1-2.jpg", 
-			"images/product1-3.jpg",
-			"images/product1-4.jpg",
-			"images/product1-5.jpg",
-			"images/product1-6.jpg",
-			"images/product1-7.jpg"
-			]}
-
+  {
+    id: 1, 
+    name: "Gauri Pujan & Mahalakshmi Acrylic Rangoli Set", 
+    description: "Complete Gauri Pujan acrylic set featuring twin divine faces, pink lotuses, samai lamps, and auspicious footprints. Reusable, washable, and mess-free for effortless Mahalakshmi and Gauri Ganpati celebrations.", 
+    price: 750, 
+    mrp: 1200, 
+    badge: "Special Deal", 
+    colors: ["Multi"],
+    size: '12" x 12"', 
+    height: "2 mm", 
+    weight: "100g", 
+    images: [
+      "images/product1-1.jpg", 
+      "images/product1-2.jpg", 
+      "images/product1-3.jpg",
+      "images/product1-4.jpg",
+      "images/product1-5.jpg",
+      "images/product1-6.jpg",
+      "images/product1-7.jpg"
+    ]
+  },
+  {
+    id: 2, 
+    name: "Lotus Acrylic Rangoli", 
+    description: "Beautiful lotus acrylic rangoli with vibrant green leaves, perfect for Mahalakshmi Pujan, Gauri Pujan, Diwali, Ganesh Chaturthi, and festive home decoration. Reusable, washable, and mess-free for easy and elegant traditional decor.", 
+    price: 70, 
+    mrp: 110, 
+    badge: "Special Deal", 
+    colors: ["Red", "Pink", "Yellow"],
+    size: '12" x 12"', 
+    height: "2 mm", 
+    weight: "10g", 
+    images: [
+      "images/product2-1.jpg", 
+      "images/product2-2.jpg",
+	  "images/product2-3.jpg"
+    ]
+  }
 ];
