29b256b895b5206d3f505ccf0e79071ab3ffcf57
sanketkhardkar <86860065+sanketkhardkar@users.noreply.github.com>
2026-08-21 14:56:21 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,22 @@
+// products.js
+const products = [
+	{id: 1, 
+		name: "Gauri Pujan & Mahalakshmi Acrylic Rangoli Set", 
+		description: "Commemorate festive traditions with this complete Gauri Pujan floor art set. Featuring beautifully detailed twin Gauri faces, holding pink lotuses, divine footprints, samai lamps, and auspicious Devanagari calligraphy. Reusable, washable, and designed for effortless setup during Mahalakshmi & Gauri Ganpati celebrations.", 
+		price: 750, 
+		mrp: 1200, 
+		badge: "Special Deal", 
+		size: '12" x 12"', 
+		height: "2 mm", 
+		weight: "100g", 
+		images: [
+			"images/product1-1.jpg", 
+			"images/product1-2.jpg", 
+			"images/product1-3.jpg",
+			"images/product1-4.jpg",
+			"images/product1-5.jpg",
+			"images/product1-6.jpg",
+			"images/product1-7.jpg"
+			]}
+
+];
