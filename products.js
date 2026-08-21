fa49a9caae574a7480e56bdf0a6565663460be3f
sanketkhardkar <86860065+sanketkhardkar@users.noreply.github.com>
2026-08-21 22:23:50 +0530

    Add files via upload

--- products.js
+++ products.js
@@ -1,41 +1,23 @@
 // products.js
 const products = [
-  {
-    id: 1, 
-    name: "Gauri Pujan & Mahalakshmi Acrylic Rangoli Set", 
-    description: "Complete Gauri Pujan acrylic set featuring twin divine faces, pink lotuses, samai lamps, and auspicious footprints. Reusable, washable, and mess-free for effortless Mahalakshmi and Gauri Ganpati celebrations.", 
-    price: 750, 
-    mrp: 1200, 
-    badge: "Special Deal", 
-    colors: ["Multi"],
-    size: '12" x 12"', 
-    height: "2 mm", 
-    weight: "100g", 
-    images: [
-      "images/product1-1.jpg", 
-      "images/product1-2.jpg", 
-      "images/product1-3.jpg",
-      "images/product1-4.jpg",
-      "images/product1-5.jpg",
-      "images/product1-6.jpg",
-      "images/product1-7.jpg"
-    ]
-  },
-  {
-    id: 2, 
-    name: "Lotus Acrylic Rangoli", 
-    description: "Beautiful lotus acrylic rangoli with vibrant green leaves, perfect for Mahalakshmi Pujan, Gauri Pujan, Diwali, Ganesh Chaturthi, and festive home decoration. Reusable, washable, and mess-free for easy and elegant traditional decor.", 
-    price: 70, 
-    mrp: 110, 
-    badge: "Trending Now", 
-    colors: ["Red", "Pink", "Yellow"],
-    size: '12" x 12"', 
-    height: "2 mm", 
-    weight: "10g", 
-    images: [
-      "images/product2-1.jpg", 
-      "images/product2-2.jpg",
-	  "images/product2-3.jpg"
-    ]
-  }
+	{id: 1, name: "Gauri Pujan & Mahalakshmi Acrylic Rangoli Set", 
+	description: "Complete Gauri Pujan acrylic set featuring twin divine faces, pink lotuses, samai lamps, and auspicious footprints. Reusable, washable, and mess-free for effortless Mahalakshmi and Gauri Ganpati celebrations.", 
+	price: 1000, mrp: 1500, badge: "Special Deal", colors: ["Multi"], size: '45 x 45 cm', height: "2 mm", weight: "100g", 
+	images: [ "images/product1-1.jpg", "images/product1-2.jpg", "images/product1-3.jpg", "images/product1-4.jpg", "images/product1-5.jpg", "images/product1-6.jpg", "images/product1-7.jpg" ]},
+
+	{id: 2, name: "Lotus Acrylic Rangoli (Big Size)", 
+	description: "Beautiful lotus acrylic rangoli with vibrant green leaves, perfect for Mahalakshmi Pujan, Gauri Pujan, Diwali, Ganesh Chaturthi, and festive home decoration. Reusable, washable, and mess-free for easy and elegant traditional decor.", 
+	price: 60, mrp: 100, badge: "Trending Now", colors: ["Red", "Pink", "Yellow"], size: '12 x 12 cm', height: "2 mm", weight: "10g", 
+	images: [ "images/product2-1.jpg", "images/product2-2.jpg", "images/product2-3.jpg" ]},
+	
+	{id: 3, name: "Lotus Acrylic Rangoli (Small Size)", 
+	description: "Beautiful lotus acrylic rangoli with vibrant green leaves, perfect for Mahalakshmi Pujan, Gauri Pujan, Diwali, Ganesh Chaturthi, and festive home decoration. Reusable, washable, and mess-free for easy and elegant traditional decor.", 
+	price: 40, mrp: 70, badge: "Trending Now", colors: ["Red", "Pink", "Yellow"], size: '9 x 9 cm', height: "2 mm", weight: "10g", 
+	images: [ "images/product2-1.jpg", "images/product2-2.jpg", "images/product2-3.jpg" ]},
+	
+	{id: 4, name: "Lotus Entrance Border Strips (10 Pcs)", 
+	description: "Enhance your home with this 10-piece Lotus Acrylic Doorstep Border Rangoli. Featuring vibrant lotus motifs with green leaf accents, this reusable, washable set brings instant festive elegance to your entrance, puja space, and home celebrations without any mess.", 
+	price: 300, mrp: 450, badge: "New Arrival", colors: [ "Multi"], size: '10 x 100 cm', height: "2 mm", weight: "10g", 
+	images: [ "images/product4-1.jpg", "images/product4-2.jpg" ]}
+
 ];
